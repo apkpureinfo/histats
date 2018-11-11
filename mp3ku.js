03d270e73f8813bd0d3de1c71e5eee32a5a74ece
apkpureinfo <36444974+apkpureinfo@users.noreply.github.com>
2018-11-11 16:02:38 +0800

    Create mp3ku.js

---
+++ mp3ku.js
@@ -0,0 +1,9 @@
+var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.startgif', '1,4176180,4,10047,"div#histatsC {position: absolute;top:0px;left:0px;}body>div#histatsC {position: fixed;}"']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_gif_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
